fd2ad852f843700e0824520f8aaf6be4a0572f5c
Nikita Grebnev <n.a.grebnev@gmail.com>
2025-01-05 01:25:28 +0500

    Тестовый диалог

---
+++ Assets/Scripts/ConversationStarter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using DialogueEditor;
+using UnityEngine;
+
+public class ConversationStarter : MonoBehaviour {
+    [SerializeField] private NPCConversation conversation;
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player")) {
+            if (Input.GetKeyDown(KeyCode.F)) {
+                ConversationManager.Instance.StartConversation(conversation);
+            }
+        }
+    }
+}
